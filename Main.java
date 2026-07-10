3385fa018d3b435900ab2d1a70340cde1a66ab16
Andreakuno <dreyana.andrea06@gmail.com>
2026-07-10 19:32:53 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        StudentLogIn s = new StudentLogIn();
-
-
-        System.out.println("Enter Username: ");
+        System.out.print("Enter Username: ");
         String username = sc.nextLine();
-        System.out.println("Enter Year Level: ");
-        int yearLevel = sc.nextInt();
-        System.out.println("Enter Course: ");
-        String course = sc.next();
+        System.out.print("Enter Year Level: ");
 
+        while (!sc.hasNextInt()) {
+            System.out.println("Invalid input! Please enter a number.");
+            sc.next(); 
+            System.out.print("Enter Year Level: ");
+        }
+        int yearLevel = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Course: ");
+        String course = sc.nextLine();
+
+        StudentLogIn s = new StudentLogIn(username, yearLevel, course);
+
+        s.StudentLogin();
     }
 }
